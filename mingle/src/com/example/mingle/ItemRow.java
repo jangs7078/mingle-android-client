705d646b0dad12f12364fd090c3a29a1862e42a9
dokwon <dokwon@stanford.edu>
2014-06-27 11:19:17 +0900

    first commit

---
+++ mingle/src/com/example/mingle/ItemRow.java
@@ -0,0 +1,31 @@
+package com.example.mingle;
+
+import android.graphics.drawable.Drawable;
+
+public class ItemRow {
+	String itemName;
+    Drawable icon;
+
+    public ItemRow(String itemName, Drawable icon) {
+          super();
+          this.itemName = itemName;
+          this.icon = icon;
+    }
+    
+    public String getItemName() {
+          return itemName;
+    }
+    
+    public void setItemName(String itemName) {
+          this.itemName = itemName;
+    }
+    
+    public Drawable getIcon() {
+          return icon;
+    }
+    
+    public void setIcon(Drawable icon) {
+          this.icon = icon;
+    }
+
+}
